1402a3e47cca21d561d13fab8e097e274244f2c0
DESKTOP-SCHIQEO\mertk <mertkasim2019@gmail.com>
2022-11-27 17:50:04 +0300

    SQL connection bug fix
    "FATAL: sorry, too many clients already" error is fixed

--- go-backend/infrastructure/database/gormrepo/database.go
+++ go-backend/infrastructure/database/gormrepo/database.go
@@ -6,6 +6,8 @@ import (
 	dbmodels "go-backend/models/dbmodels"
 	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -34,6 +36,27 @@ func GetDatabase() *gorm.DB {
 		} else {
 			log.Println("Database created successfully")
 		}
+
+		maxIdleConnStr := os.Getenv(config.DB_MAX_IDLE_CONNS)
+		maxIdleConn, maxIdleConnErr := strconv.Atoi(maxIdleConnStr)
+
+		maxOpenConnStr := os.Getenv(config.DB_MAX_IDLE_CONNS)
+		maxOpenConn, maxOpenConnErr := strconv.Atoi(maxOpenConnStr)
+
+		connMaxLifetimeStr := os.Getenv(config.DB_CONN_MAX_LIFETIME)
+		connMaxLifetime, connMaxLifetimeErr := strconv.Atoi(connMaxLifetimeStr)
+
+		if maxIdleConnErr != nil || maxOpenConnErr != nil || connMaxLifetimeErr != nil {
+			log.Fatalln("error on getting or parsing DB_MAX_IDLE_CONNS, DB_MAX_OPEN_CONNS, DB_CONN_MAX_LIFETIME")
+		}
+
+		sqlDB, sqldbErr := db.DB()
+		if sqldbErr != nil {
+			log.Fatalln("sql db error", sqldbErr)
+		}
+		sqlDB.SetMaxIdleConns(maxIdleConn)
+		sqlDB.SetMaxOpenConns(maxOpenConn)
+		sqlDB.SetConnMaxLifetime(time.Duration(connMaxLifetime) * time.Second)
 	}
 
 	return db
